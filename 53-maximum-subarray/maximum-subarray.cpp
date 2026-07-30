4a9e5fdb6b7ae4027b248e04e329082efe1a50a9
abdulRahman <abdulrahman.mgitcse@gmail.com>
2026-07-30 22:12:29 +0530

    Time: 1 ms (25.12%) | Memory: 71.8 MB (53.22%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,9 +5,7 @@ public:
         long long sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum>maxi){
-                maxi=sum;
-            }
+            maxi=max(maxi,sum);
             if(sum<0){
                 sum=0;
             }
