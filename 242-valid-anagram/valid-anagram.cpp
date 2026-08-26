0da617a32b00ad2fa058594c6e07782c608ac36d
abdulRahman <abdulrahman.mgitcse@gmail.com>
2026-08-26 23:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -6,11 +6,11 @@ public:
         int n2=t.size();
         if(n1!=n2)return false;
         for(int i=0;i<n1;i++){
-            mpp[s[i]-'a']++;
+            mpp[s[i]]++;
         }
         for(int i=0;i<n2;i++){
-            mpp[t[i]-'a']--;
-            if(mpp[t[i]-'a']<0)return false;
+            mpp[t[i]]--;
+            if(mpp[t[i]]<0)return false;
         }
         return true;
     }
